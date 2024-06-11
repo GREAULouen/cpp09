d786068d2dc6148881b4dcb0d8c30e50f29cd552
Louen Greau <lgreau@1-A-18.42heilbronn.de>
2024-06-11 21:51:14 +0200

    Basic input argument handling

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,18 +6,21 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 21:13:26 by lgreau            #+#    #+#             */
-/*   Updated: 2024/06/11 21:15:37 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/06/11 21:50:10 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
+	if (argc != 2)
 	{
-		RPN	rpn = RPN();
-		std::string input = "8 9 * 9 - 9 - 9 - 4 - 1 +";
-		rpn.compute(input);
+		std::cerr	<< "\033[0;31m" << "Error: the program takes one and only one Reverse Polish Notation string"
+				<< "\033[0m" << std::endl;
+		return 1;
 	}
+	RPN	rpn = RPN();
+	rpn.compute(argv[1]);
 	return 0;
 }
