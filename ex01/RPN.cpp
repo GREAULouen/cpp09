f6b884ecdfbf7e1f194c695bab5544766e5012d5
Louen Greau <lgreau@1-A-18.42heilbronn.de>
2024-06-11 21:30:33 +0200

    Integer extraction

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 20:44:56 by lgreau            #+#    #+#             */
-/*   Updated: 2024/06/11 21:11:36 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/06/11 21:30:16 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,27 @@ RPN &	RPN::operator=(RPN const &toAssign)
 void	RPN::compute(std::string input)
 {
 	std::cout << "input: " << input << std::endl;
-	std::ostringstream	oss(input, std::ostringstream::in);
+	std::stringstream	ss(input, std::stringstream::in);
 	std::string	operators[4] = {"+", "-", "/", "*"};
 
-	size_t	next_op = input.npos;
-	while (true)
+	int	next_op;
+	int	value;
+	while (ss >> input) // Using the ss to "move" the input char by char
 	{
 		next_op = which_of(input, operators, 4);
 		if (next_op == -1)
 		{
-			// Numeric value
-			std::cout << "  |- next_val : " << oss.str();
 			// Convert to int then stack up
+			value = static_cast<int>(std::strtol(input.substr(0, 1).c_str(), NULL, 10));
+			std::cout << "  |- value    : " << value << std::endl;
+			this->_tokens.push(value);
 		} else {
 			std::cout << "  |- next_op  : " << operators[next_op] << std::endl;
 			// Verify that there's at least 2 operands in the stack
 			// Retrieve the first 2 operands from the stack
 			// Execute the operation on them
 			// Stack up the result form the operation
+			break ;
 		}
 
 	}
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 20:38:58 by lgreau            #+#    #+#             */
-/*   Updated: 2024/06/11 21:08:18 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/06/11 21:30:21 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 class RPN
 {
 	private:
-		std::stack<std::string>	_tokens;
+		std::stack<int>	_tokens;
 
 	public:
 		RPN();
@@ -33,6 +33,6 @@ class RPN
 		void	compute(std::string input);
 };
 
-int	which_of(std::string input, std::string *arr, int arr_size);
+int		which_of(std::string input, std::string *arr, int arr_size);
 
 #endif
