106a8ebebf888b7c6c5cf12f527c1f34a91ea057
Louen Greau <lgreau@1-A-18.42heilbronn.de>
2024-06-11 21:48:01 +0200

    Operations handling & some error checking

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 20:44:56 by lgreau            #+#    #+#             */
-/*   Updated: 2024/06/11 21:30:16 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/06/11 21:47:27 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,12 @@ void	RPN::compute(std::string input)
 	std::cout << "input: " << input << std::endl;
 	std::stringstream	ss(input, std::stringstream::in);
 	std::string	operators[4] = {"+", "-", "/", "*"};
+	int (*operations[4])(int, int) = {&rpn_add, &rpn_sub, &rpn_div, &rpn_mult};
 
 	int	next_op;
 	int	value;
+	int	op_a;
+	int	op_b;
 	while (ss >> input) // Using the ss to "move" the input char by char
 	{
 		next_op = which_of(input, operators, 4);
@@ -43,14 +46,34 @@ void	RPN::compute(std::string input)
 			this->_tokens.push(value);
 		} else {
 			std::cout << "  |- next_op  : " << operators[next_op] << std::endl;
+
 			// Verify that there's at least 2 operands in the stack
+			if (this->_tokens.size() < 2)
+			{
+				std::cerr << "Error: " << operators[next_op] << " execution impossible: Not enough operands" << std::endl;
+				return ;
+			}
+
 			// Retrieve the first 2 operands from the stack
+			op_b = this->_tokens.top();
+			this->_tokens.pop();
+			op_a = this->_tokens.top();
+			this->_tokens.pop();
+
 			// Execute the operation on them
 			// Stack up the result form the operation
-			break ;
+			this->_tokens.push(operations[next_op](op_a, op_b));
 		}
-
 	}
+
+	// Verify that there's no leftover operands
+	if (this->_tokens.size() > 1)
+	{
+		std::cerr << "Error: " << this->_tokens.size() << " too much operands" << std::endl;
+		return ;
+	}
+
+	std::cout << " => " << this->_tokens.top() << std::endl;
 }
 
 
@@ -65,3 +88,24 @@ int	which_of(std::string input, std::string *arr, int arr_size)
 			return i;
 	return -1;
 }
+
+int	rpn_add(int a, int b)
+{
+	return a + b;
+}
+
+int	rpn_sub(int a, int b)
+{
+	return a - b;
+}
+
+int	rpn_div(int a, int b)
+{
+	return a / b;
+}
+
+int	rpn_mult(int a, int b)
+{
+	return a * b;
+}
+
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 20:38:58 by lgreau            #+#    #+#             */
-/*   Updated: 2024/06/11 21:30:21 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/06/11 21:33:44 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,9 @@ class RPN
 };
 
 int		which_of(std::string input, std::string *arr, int arr_size);
+int		rpn_add(int a, int b);
+int		rpn_sub(int a, int b);
+int		rpn_div(int a, int b);
+int		rpn_mult(int a, int b);
 
 #endif
