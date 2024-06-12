e952bc419b66c0fc7638ded49efc79753d45a440
Louen Greau <lgreau@1-A-18.42heilbronn.de>
2024-06-12 17:20:21 +0200

    Added output as requested by the subject

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:22:09 by lgreau            #+#    #+#             */
-/*   Updated: 2024/06/12 17:06:32 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/06/12 17:19:48 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,14 +46,40 @@ int	main(int argc, char *argv[])
 			v.push_back(tmp);
 		}
 
+		// Sequence pre-sort
 		std::cout << "Before : ";
 		print<std::list<int> >(l);
 
+		clock_t	start_list = std::clock();
 		PmergeMe<std::list<int> >::mi_sort(l);
-		PmergeMe<std::vector<int> >::mi_sort(v);
+		double	time_list = static_cast<double>(std::clock() - start_list) / CLOCKS_PER_SEC; // Convert the time it took for the list sorting to us
 
+
+		clock_t	start_vector = std::clock();
+		PmergeMe<std::vector<int> >::mi_sort(v);
+		double	time_vector = static_cast<double>(std::clock() - start_vector) / CLOCKS_PER_SEC; // Convert the time it took for the vector sorting to us
+
+		// Sequence post-sort
 		std::cout << "After  : ";
 		print<std::list<int> >(l);
+
+
+		// List benchmark result output
+		std::cout	<< "Time to process a range of "
+					<< std::setw(8) << (argc - 1)
+					<< " elements with std::list<int>\t\t: "
+					<< std::setw(12) << std::fixed << GREEN << time_list << RESET
+					<< " us"
+				<< std::endl;
+
+
+		// Vector benchmark result output
+		std::cout	<< "Time to process a range of "
+					<< std::setw(8) << (argc - 1)
+					<< " elements with std::vector<int>\t: "
+					<< std::setw(12) << std::fixed << GREEN << time_vector << RESET
+					<< " us"
+				<< std::endl;
 	}
 
 	return 0;
