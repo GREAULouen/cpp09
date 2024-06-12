f445801f470ed8da5ef66f92af79a3485e50aca7
Louen Greau <lgreau@1-A-18.42heilbronn.de>
2024-06-12 16:03:14 +0200

    Added the logic of the algorithm & fixed duplicate insertion order values creating new entry in the Container

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:09:14 by lgreau            #+#    #+#             */
-/*   Updated: 2024/06/12 14:57:48 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/06/12 16:01:41 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 # include <iostream>
 # include <algorithm>
+# include <vector>
 
 # define RED "\033[0;31m"
 # define GREEN "\033[0;32m"
@@ -25,11 +26,6 @@ template<typename Container>
 class PmergeMe
 {
 	private:
-		Container	_small;
-		Container	_large;
-
-
-	public:
 		PmergeMe() {}
 		PmergeMe(PmergeMe const &toCopy) {*this = toCopy;}
 		~PmergeMe() {}
@@ -37,43 +33,114 @@ class PmergeMe
 		PmergeMe &	operator=(PmergeMe const &toAssign)
 		{
 			if (this == &toAssign) return *this;
-			this->_small = toAssign._small;
-			this->_large = toAssign._large;
 			return *this;
 		}
 
 
-		void	mi_sort(Container &input)
+	public:
+		static void	mi_sort(Container &input)
 		{
-			if (input.size() < 1)
-			{
-				std::cerr	<< RED << "Error: the container is empty"
-						<< RESET << std::endl;
-				return ;
+			if (input.size() <= 1) return ;
+
+			// Step 1: Separate elements by pairs & put the small/large in separated containers
+			Container	small;
+			Container	large;
+			split_n_sort_pairs(input, small, large);
+
+			// Step 2: Sort the container w/ the large values RECURSIVELY
+			mi_sort(large);
+
+			// Step 3: Insert the elements from small Container back at their place in the large Container
+			//         Using a specific order
+			std::vector<typename Container::iterator> insertionOrder = getInsertionOrder(small);
+			for (size_t i = 0; i < insertionOrder.size(); ++i) {
+				typename Container::iterator pos = std::lower_bound(large.begin(), large.end(), *insertionOrder[i]);
+				large.insert(pos, *insertionOrder[i]);
 			}
+
+			// Step 4: Put the sorted values back into input Container
+			input.swap(large);
 		}
 
 		/*	~~~~~~~~~~~~~~~~ UTILS ~~~~~~~~~~~~~~~~	*/
 
-		void	split_n_sort_pairs(Container &input)
+		/**
+		 * @brief Separates the input container's elements in pairs & sorts them
+		 * (if odd: leave one alone)
+		 * @param input
+		 */
+		static void	split_n_sort_pairs(Container &input, Container &small, Container &large)
 		{
 			typename Container::iterator	it = input.begin();
 			while (it != input.end())
 			{
-				typename Container::iterator first = it++;
+				typename Container::iterator	first = it++;
 				if (it != input.end()) {
-					typename Container::iterator second = it++;
+					typename Container::iterator	second = it++;
 					if (*first < *second) {
-						this->_small.push_back(*first);
-						this->_large.push_back(*second);
+						small.push_back(*first);
+						large.push_back(*second);
 					} else {
-						this->_small.push_back(*second);
-						this->_large.push_back(*first);
+						small.push_back(*second);
+						large.push_back(*first);
 					}
 				} else {
-					this->_small.push_back(*first);
+					small.push_back(*first);
 				}
 			}
+		}
+
+		/**
+		 * @brief Generate the Jacobsthal Numbers sequence till n;
+		 *
+		 * @param n
+		 * @return std::vector<int>
+		 */
+		static std::vector<int>	generateJacobsthalNumbers(int n) {
+			std::vector<int> jacobsthal;
+			jacobsthal.push_back(0); // J(1)
+			jacobsthal.push_back(1); // J(2)
+			for (int i = 2; ; ++i) {
+				int next = jacobsthal[i-1] + 2 * jacobsthal[i-2];	// J[n + 2] = J[n + 1] + 2 * J[n]
+				if (next >= n) break;
+				jacobsthal.push_back(next);
+			}
+			return jacobsthal;
+		}
+
+		/**
+		 * @brief Get the order of small Container elements insertion
+		 *
+		 * @param small
+		 * @return std::vector<typename Container::iterator>
+		 */
+		static std::vector<typename Container::iterator> getInsertionOrder(Container& small) {
+			std::vector<typename Container::iterator> order;
+			int n = small.size();
+			if (n == 0) return order;
+
+			std::vector<int> jacobsthal = generateJacobsthalNumbers(n);
+			std::vector<bool> inserted(n, false);
+
+			for (size_t i = 1; i < jacobsthal.size(); ++i) {
+				int idx = jacobsthal[i] - 1;
+				if (idx < n && !inserted[idx]) {
+					typename Container::iterator it = small.begin();
+					std::advance(it, idx);
+					order.push_back(it);
+					inserted[idx] = true;
+				}
+			}
+
+			for (int i = 0; i < n; ++i) {
+				if (!inserted[i]) {
+					typename Container::iterator it = small.begin();
+					std::advance(it, i);
+					order.push_back(it);
+				}
+			}
+
+			return order;
 		}
 };
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:22:09 by lgreau            #+#    #+#             */
-/*   Updated: 2024/06/12 14:58:09 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/06/12 16:02:35 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,33 @@
 #include <cstdlib>
 #include <ctime>
 
+template<typename Container>
+void	print(Container &a)
+{
+	for (typename Container::iterator	it = a.begin(); it != a.end(); ++it)
+		std::cout << *it << " ";
+
+	std::cout << std::endl;
+}
+
 int main(void)
 {
 	std::srand(time(NULL));
 	{
-		std::list<int> list(10);
+		std::list<int> list(500);
 		std::generate(list.begin(), list.end(), std::rand);
 
-		PmergeMe<std::list<int> > pmerge_me = PmergeMe<std::list<int> >();
+		std::cout	<< "Before : " << ((std::is_sorted(list.begin(), list.end()))?GREEN:RED);
+		print<std::list<int> >(list);
+		std::cout	<< RESET;
 
-		pmerge_me.mi_sort(list);
+		PmergeMe<std::list<int> >::mi_sort(list);
+
+		std::cout	<< "After  : " << ((std::is_sorted(list.begin(), list.end()))?GREEN:RED);
+		print<std::list<int> >(list);
+		std::cout	<< RESET;
+
+		std::cout << "Debug: " << list.size() << " elements after mi_sort" << std::endl;
 	}
 	return 0;
 }
